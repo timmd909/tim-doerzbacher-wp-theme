29f225059e7bb0e8aae72b3496f2505976c25183
Tim Doerzbacher <tim@tim-doerzbacher.com>
2020-01-18 22:08:49 -0500

    Enable new Bootstrap 4 custom style
    Sort of broken still... but should be able to get this working
    well soon enough.

--- functions.php
+++ functions.php
@@ -14,13 +14,18 @@ function enqueue_child_theme_styles() {
 
 	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 
-	// wp_enqueue_style( 'tim-doerzbacher-wp-theme',
-	// 	get_stylesheet_directory_uri() . '/inc/css/main.css',
-	// 	array( $parent_style ),
-	// 	wp_get_theme()->get('Version')
-	// );
-	//
-	// // Disable the parent's Bootstrap v3 template
-	// wp_dequeue_style( 'dazzling-bootstrap', get_template_directory_uri() . '/inc/css/bootstrap.min.css' );
-	// wp_dequeue_style( 'dazzling-bootstrap', get_template_directory_uri() . '/inc/css/font-awesome.min.css' );
+	wp_enqueue_style( 'tim-doerzbacher-wp-theme',
+		get_stylesheet_directory_uri() . '/inc/css/main.css',
+		array( $parent_style ),
+		wp_get_theme()->get('Version')
+	);
+
+	// Disable the parent's Bootstrap v3 template
+	wp_dequeue_style( 'dazzling-bootstrap', get_template_directory_uri() . '/inc/css/bootstrap.min.css' );
+	wp_dequeue_style( 'dazzling-bootstrap', get_template_directory_uri() . '/inc/css/font-awesome.min.css' );
+}
+
+
+function dazzling_footer_info() {
+	return;
 }
